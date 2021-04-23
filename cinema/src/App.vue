17ab3da5bb108d357e3f7ed2ff2811b655950676
ahmed-com <ahmed0grwan@gmail.com>
2021-04-23 04:11:06 +0200

    based on brad style sheet

---
+++ cinema/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    
+    <Cinema v-model="seats" />
+  </div>
+</template>
+
+<script>
+import Cinema from './components/Cinema.vue'
+
+export default {
+  name: 'App',
+
+  data:function () {
+    return {
+      seats: ['occupied','','selected']
+    }
+  },
+
+  components: {
+    Cinema
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
